df94da75e7e49f9c58b2daaea4384d117dc5c059
Alex <tseplyaeff.alex@yandex.ru>
2020-04-24 11:52:36 +0900

    add ClassStack

---
+++ ClassStack/ClassStack/ClassStack.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <string>
+#include "Stack.h"
+
+using namespace std;
+
+// удалить пробелы из строки
+void DeleteSpaces(string &s) {
+	for (int i = 0; i < s.length(); i++) {
+		if (s[i] == ' ') {
+			s.erase(i, 1);
+			i--;
+		}
+	}
+}
+
+int main()
+{
+	setlocale(0, "");
+	Stack<char> stack;
+	bool isPalindrome = true;
+	string str;
+	char symbol;
+	cout << "Введите строку" << endl;
+	getline(cin, str);
+	DeleteSpaces(str); // очистить строку от пробелов
+
+	try 
+	{
+		// поместить символы выражения без пробелов в стек
+		int i = 0;
+		while (str[i] != 0)
+		{
+			stack.Push(str[i]);
+			i++;
+		}
+
+		i = 0;
+		while (!stack.isEmpty()) {
+			symbol = stack.Pop();
+			if (symbol != str[i]) {
+				isPalindrome = false;
+				break;
+			}
+			i++;
+		}
+
+		if (isPalindrome) {
+			cout << "Строка является палиндромом" << "\n\n";
+		}
+
+	}
+	catch (Stack<char>::ERRORS e) 
+	{
+		if (e == Stack<char>::stack_isEmpty)
+		{
+			cout << "Стек пуст!" << endl;
+		}
+		if (e == Stack<char>::stack_overflow)
+		{
+			cout << "Стек перполнен! Добавить данные невозможно." << endl;
+		}
+	}
+	
+
+	system("pause");
+	return 0;
+}
---
+++ ClassStack/ClassStack/Stack.h
@@ -0,0 +1,63 @@
+#pragma once
+
+const unsigned MAX_STACK_SIZE = 50; // максимальная длина стека
+
+template <typename T>
+class Stack 
+{
+	private:
+		T stack_list[MAX_STACK_SIZE]; // массив из данных стека
+		int top; // индекс вершины стека
+
+	public:
+		// Исключения
+		enum ERRORS {
+			stack_overflow = -1, // переполнение стека
+			stack_isEmpty = -2, // стеку пуст
+		};
+
+		// конструктор инициализирует вершину стека
+		Stack() : top(-1) { }
+
+		// добавить элемент в вершину
+		void Push(T item) {
+			// если стек полный, завершить выполнение программы
+			if (top == MAX_STACK_SIZE - 1)
+				throw stack_overflow;
+			// иначе, увеличить вершину на 1
+			top++;
+			//добавить элемент в массив
+			stack_list[top] = item;
+		}
+
+		// изъять элемент из вершины
+		T Pop(void) {
+			T temp;
+			// если стек пуст
+			if (top == -1)
+				throw stack_isEmpty;
+			temp = stack_list[top];
+			// уменьшить длину стека на 1
+			top--;
+			// вернуть элемент из вершины стека
+			return temp;
+		}
+
+		// очистить стек
+		void Clear(void) {
+			top = -1;
+		}
+
+		// вернуть элемент, не удаляя из вершины
+		T Peek(void) const {
+			if (top == -1)
+				throw stack_isEmpty;
+			return stack_list[top];
+		}
+
+		// проверка на пустоту
+		bool isEmpty() const {
+			// если стек пуст, вернуть true, иначе false
+			return top == -1 ? true : false;
+		}
+};
